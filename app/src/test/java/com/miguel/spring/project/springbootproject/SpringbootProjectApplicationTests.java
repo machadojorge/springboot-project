c8c92dc92db644ea61f27eae255ebc5d84e2424f
Jorge Machado <miguelj1992@outlook.com>
2023-06-11 23:37:04 +0100

    First Commit

---
+++ app/src/test/java/com/miguel/spring/project/springbootproject/SpringbootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miguel.spring.project.springbootproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
