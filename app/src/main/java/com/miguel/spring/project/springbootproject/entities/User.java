0625cd9a8cc2338dca339fd4e75eae490255f378
Jorge Machado <miguelj1992@outlook.com>
2023-06-12 23:12:06 +0100

    Add H2 BD and JPA Hibernate

--- app/src/main/java/com/miguel/spring/project/springbootproject/entities/User.java
+++ app/src/main/java/com/miguel/spring/project/springbootproject/entities/User.java
@@ -2,10 +2,21 @@ package com.miguel.spring.project.springbootproject.entities;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+// we need put in this class annotations to connect this class to JPA BD
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable{
 
     private static final long serialVersionID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
